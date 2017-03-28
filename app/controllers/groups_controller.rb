4b3177a07c85ada458228f3df01fd02c774ebc7e
xuwenyu <xuwenyu@duang.sh>
2017-03-28 19:34:43 +0800

    updates in chapter7

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,6 +24,7 @@ class GroupsController < ApplicationController
     @group.user = current_user
 
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -43,6 +44,33 @@ class GroupsController < ApplicationController
     @group.destroy
     redirect_to groups_path, alert: "Group deleted"
   end
+
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论版成功！"
+    else
+      flash[:warning] = "你已经是本讨论版的成员了！！！"
+    end
+
+    redirect_to groups_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论版！！！"
+    else
+      flash[:warning] = "你不是本讨论版成员，怎么退出？？！！"
+    end
+
+    redirect_to groups_path(@group)
+  end
+
 
   private
 
